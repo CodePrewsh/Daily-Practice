0a6c7f714d7a102bec807a0d09d8177b9c13b57a
Precious Thandeka Bhembe <bhembepreciouspt@gmail.com>
2025-03-08 11:08:09 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        System.out.println("Hello World\n!");
+        System.out.println("Hello World of Java!\n");
+        System.out.println("Hello World!");
+
+        String name = "Precious";
+        String age = "21";
+        String gender = "Male";
+        String Car = "Hyundai i30N";
+
+        System.out.println("HelloI am " + name + " " + age + " " + gender + " " + Car);
+
+
+        double salary = 10000.00;
+        double bonus = 3.5;
+
+        char grade = 'A';
+        char Symbolo = '!';
+        char currency ='$';
+
+        boolean isStudent = true;
+        boolean isEnglish = true;
+        boolean isMath = false;
+
+int bro = 25;
+int 
+
     }
 }
